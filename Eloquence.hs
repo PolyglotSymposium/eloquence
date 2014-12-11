cc6aac51320644104c32d78055fe244a3839802d
Michael Gilliland <mjg.py3@gmail.com>
2014-12-11 00:47:11 -0500

    Saved "it" as result of execution in repl

--- Eloquence.hs
+++ Eloquence.hs
@@ -4,15 +4,16 @@ import Lisp (execute, executeText, parse, tokenize, bind, DataType(AList, Atom))
 import System.IO
 
 next env text = case (parse.tokenize) text of
-  (AList [Atom "def", Atom name, v]) -> bind [name] [execute env [v]] env
+  (AList [Atom "def", Atom name, v]) -> (name, execute env [v]):env
   _ -> env
 
 repl env = do
   putStr "elo> "
   hFlush stdout
   line <- getLine
-  putStrLn $ show $ executeText env line
-  repl $ next env line
+  let executedLine = executeText env line
+  putStrLn $ show $ executedLine
+  repl $ (("it", executedLine):next env line)
 
 main :: IO ()
 main = repl []
